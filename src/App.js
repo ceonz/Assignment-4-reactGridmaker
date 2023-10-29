bb669364ba96a1692c0ea5fd8c88cec00bd491d0
Ellahi <87584757+Ellahinator@users.noreply.github.com>
2023-10-29 17:16:13 -0400

    implement dynamic color picker button styling (#7)

--- src/App.js
+++ src/App.js
@@ -59,8 +59,28 @@ export default function App() {
   }
 
   const changeColor = (event) => {
-    setColor(event.target.value);
-  }
+    const selectedColor = event.target.value;
+    setColor(selectedColor);
+    
+    const colorPickerButton = document.getElementById("colorPickerButton");
+    colorPickerButton.style.backgroundColor = selectedColor;
+
+    // Dynamically change text color based on button background color
+    const rgb = parseInt(selectedColor.substring(1), 16);   
+    const r = (rgb >> 16) & 0xff;  
+    const g = (rgb >>  8) & 0xff;  
+    const b = (rgb >>  0) & 0xff; 
+
+    const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+    if (luma > 230) {  
+      colorPickerButton.style.boxShadow = "inset 0 0 0 1px #007BFF";
+    } else {
+      colorPickerButton.style.boxShadow = "none";
+    }
+
+    colorPickerButton.style.color = luma < 128 ? 'white' : 'black';
+  };
 
   const onClickCell = (event) => {
     event.target.style.backgroundColor = color;

e9c76b72542eac9622aa26482eecce10b7053177
ceonz <ceon91@gmail.com>
2023-11-01 11:30:22 -0400

    add color toggle mode

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function Grid({ grid, onClick }) {
       {grid.map((row, rowIndex) => (
         <div className="board-row" key={rowIndex}>
           {row.map((cellColor, cellIndex) => (
-            <button className="square" key={cellIndex} style={{backgroundColor: cellColor}} onClick={() => onClick(rowIndex, cellIndex)}>
+            <button className="square" key={cellIndex} style={{ backgroundColor: cellColor }} onClick={() => onClick(rowIndex, cellIndex)}>
             </button>
           ))}
         </div>
@@ -15,10 +15,16 @@ function Grid({ grid, onClick }) {
   );
 }
 
+const MODE = {
+  Single: "SINGLE",
+  Cross: "CROSS",
+}
+
 export default function App() {
   const [grid, setGrid] = useState([]);
   const [color, setColor] = useState("#007BFF");
-  const[cellsColored, setCellsColored] = useState(false);
+  const [cellsColored, setCellsColored] = useState(false);
+  const [colorMode, setColorMode] = useState(MODE.Single);
 
   const rows = grid.length;
   const columns = grid[0]?.length || 0;
@@ -61,19 +67,19 @@ export default function App() {
   const changeColor = (event) => {
     const selectedColor = event.target.value;
     setColor(selectedColor);
-    
+
     const colorPickerButton = document.getElementById("colorPickerButton");
     colorPickerButton.style.backgroundColor = selectedColor;
 
     // Dynamically change text color based on button background color
-    const rgb = parseInt(selectedColor.substring(1), 16);   
-    const r = (rgb >> 16) & 0xff;  
-    const g = (rgb >>  8) & 0xff;  
-    const b = (rgb >>  0) & 0xff; 
+    const rgb = parseInt(selectedColor.substring(1), 16);
+    const r = (rgb >> 16) & 0xff;
+    const g = (rgb >> 8) & 0xff;
+    const b = (rgb >> 0) & 0xff;
 
     const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
-    if (luma > 230) {  
+    if (luma > 230) {
       colorPickerButton.style.boxShadow = "inset 0 0 0 1px #007BFF";
     } else {
       colorPickerButton.style.boxShadow = "none";
@@ -83,8 +89,18 @@ export default function App() {
   };
 
   const onClickCell = (rowIndex, cellIndex) => {
-    const newGrid =[...grid];
-    newGrid[rowIndex][cellIndex] = color;
+    const newGrid = [...grid];
+    if (colorMode === MODE.Single) {
+      newGrid[rowIndex][cellIndex] = color;
+    } else if (colorMode === MODE.Cross) {
+      for (let i = 0; i < newGrid.length; i++) {
+        for (let j = 0; j < newGrid[i].length; j++) {
+          if (i == rowIndex || j == cellIndex) {
+            newGrid[i][j] = color;
+          }
+        }
+      }
+    }
     setGrid(newGrid);
   }
 
@@ -106,6 +122,10 @@ export default function App() {
     setGrid(newGrid);
   }
 
+  const toggleColorMode = () => {
+    setColorMode(colorMode === MODE.Single ? MODE.Cross : MODE.Single);
+  }
+
   return (
     <div className="game">
       <div className="menus">
@@ -115,14 +135,15 @@ export default function App() {
         <button onClick={removeRow}>Remove Row</button>
         <button onClick={removeColumn}>Remove Column</button>
         <div className="color-button-wrapper">
-            <button id="colorPickerButton" className="color-button">Pick Color</button>
-            <input id="colorPicker" type="color" className="hidden-color-picker" onInput={changeColor}/>
+          <button id="colorPickerButton" className="color-button">Pick Color</button>
+          <input id="colorPicker" type="color" className="hidden-color-picker" onInput={changeColor} />
         </div>
         <button onClick={colorAll}>Color All Cells</button>
         <button onClick={colorAllUncolored}>Color All Uncolored</button>
         <button onClick={removeColors}>Remove Color</button>
+        <button onClick={toggleColorMode}>Mode: {colorMode}</button>
       </div>
-      <Grid grid={grid} onClick={onClickCell}/>
+      <Grid grid={grid} onClick={onClickCell} />
     </div>
   );
 }
